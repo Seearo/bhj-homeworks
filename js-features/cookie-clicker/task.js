b47cdea34cd347b836f0b44faac258030b6b02a4
SeearoTV <seearotv@gmail.com>
2023-09-17 10:24:08 +0300

    new

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -8,15 +8,19 @@ let size = 0;
 function countByClick() {
 
     if (size == 0) {
+
         size++;
         cookie.width = 300;
         cookie.height = 300;
         clickCounter.textContent++;
+
     } else if (size == 1) {
+
         size--;
         cookie.width = 200;
         cookie.height = 200;
         clickCounter.textContent++;
+        
     }
 }
 
